3b05a48c08d90fe19ed4180f09a72bcd17b6479a
TheGamer_Geek <wellernd@gmail.com>
2024-02-01 15:52:46 -0500

    finished roulette logic and css styling

--- html/roulette.js
+++ html/roulette.js
@@ -1,9 +1,9 @@
+"use strict"
 
 
-
-roulette = [0, 34, 10, 21, 28, 4, 18, 9, 27, 22, 12, 3, 17, 20, 11, 33, 2, 10, 32, "00", 15, 8, 25, 1, 31, 20, 14, 30, 7, 24, 29, 35, 6, 13, 23, 19, 5, 36];
-number = ["zéro", "Trente-quatre", "dix", "vingt et un", "vingt-huit", "quatre", "dix-huit", "neuf", "vingt-sept", "vingt-deux", "douze", "trois", "dix-sept", 
-"vingt", "onze", "Trente-trois", "deux", "dix", "Trente-deux", "double zéro", "quinze", "huit", "vingt-cinq", "un", "Trente et un", "vingt", "quatorze", "trente", 
+let roulette = [0, 34, 10, 21, 28, 4, 18, 9, 27, 22, 12, 3, 17, 20, 11, 33, 2, 10, 32, "00", 15, 8, 25, 1, 31, 20, 14, 30, 7, 24, 29, 35, 6, 13, 23, 19, 5, 36];
+let number = ["zero", "Trente-quatre", "dix", "vingt et un", "vingt-huit", "quatre", "dix-huit", "neuf", "vingt-sept", "vingt-deux", "douze", "trois", "dix-sept", 
+"vingt", "onze", "Trente-trois", "deux", "dix", "Trente-deux", "double zero", "quinze", "huit", "vingt-cinq", "un", "Trente et un", "vingt", "quatorze", "trente", 
 "sept", "vingt-quatre", "vingt-neuf", "Trente-cinq", "six", "treize", "vingt-trois", "dix-neuf", "cinq", "Trente-six"];
 
 function forp(val)
@@ -23,11 +23,11 @@ function col(val)
 {
     if (val % 2 == 0)
     {
-        return "rouge, ";
+        return "rouge";
     }
     else
     {
-        return "noir, ";
+        return "noir";
     }
 }
 
@@ -35,35 +35,33 @@ function even(val)
 {
     if (val % 2 == 0)
     {
-        return "pair, ";
+        return "pair";
     }
     else
     {
-        return "impair, ";
+        return "impair";
     }
 }
 
 function spin() 
 {
-    randspin = Math.floor(Math.random() * 38);
+    let randspin = Math.floor(Math.random() * 38);
 
-    rand = roulette[randspin];
+    let rand = roulette[randspin];
 
-    if (rand == "00")
+    let spinValue = [number[randspin], col(randspin), even(rand), forp(rand)];
+    let spin = number[randspin] + col(randspin) + even(rand) + forp(rand);
+    let elem = document.createElement("tr");
+    elem.className = "row";
+
+    for (let i = 0; i < 4; i++)
     {
-        return "double zéro, noir, pair, passe";
+        let data = document.createElement("td");
+        data.appendChild(document.createTextNode(spinValue[i]));
+        data.className = spinValue[i];
+        elem.appendChild(data);
     }
-    else if (rand == 0)
-    {
-        return "zéro, rouge, impair, and manque";
-    }
-    else 
-    {
-        spin = number[randspin] + ", " + col(randspin) + even(rand) + forp(rand);
-        elem = document.createElement("roulette");
-        document.createTextNode(”…”);
-        console.log(spin);
-    }
+
+    let table = document.getElementById("roulette");
+    table.appendChild(elem);
 }
-
-spin();
